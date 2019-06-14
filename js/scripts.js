11b2492cb7949cdc6d1ac3454f11fd1ac498e451
Max T <max@23cm.io>
2019-06-14 19:39:37 +0600

    сделал разметку деккоративных элементов

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+var hotelSearchButton = document.querySelector(".booking-promo-button");
+var searchPopup = document.querySelector(".booking-form");
+var form = searchPopup.querySelector("form");
+
+hotelSearchButton.addEventListener("click",
+function (evt) {
+  evt.preventDefault();
+  console.log("hello world");
+  searchPopup.classList.toggle("modal-hide");
+});
+
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+});
+
+
+
+
+
+function initMap() {
+  var myLatLng = {lat: 34.876142, lng: -111.756358};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 9,
+    center: myLatLng,
+    disableDefaultUI: true
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Welcome to Sedona!'
+  });
+}
