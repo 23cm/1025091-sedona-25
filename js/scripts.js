fbcf4c40855afd3854ff67849fd1aea61b44a89c
Max T <max@23cm.io>
2019-06-15 23:10:05 +0600

    поправил svg и спрятал форму

--- js/scripts.js
+++ js/scripts.js
@@ -4,64 +4,55 @@ var form = searchPopup.querySelector("form");
 var checkInDate = form.querySelector("#checkInDate");
 var checkOutDate = form.querySelector("#checkOutDate");
 var adultsQty = form.querySelector("#adultsQty");
-var childsQty =form.querySelector("#childsQty");
+var childsQty = form.querySelector("#childsQty");
 
 var isStorageSupport = true;
 var storage = "";
-
 try {
   storage = localStorage.getItem("login");
 } catch (err) {
   isStorageSupport = false;
 }
 
-hotelSearchButton.addEventListener("click",
-function (evt) {
+searchPopup.classList.add("modal-hide");
+
+hotelSearchButton.addEventListener("click", function(evt) {
   evt.preventDefault();
   searchPopup.classList.toggle("modal-hide");
 });
 
-form.addEventListener("submit", function (evt) {
+form.addEventListener("submit", function(evt) {
   if (!checkInDate.value) {
-  evt.preventDefault();
-  console.log('Укажите дату заезда!');
-}
-else {
-  localStorage.setItem("adultsQty", adultsQty.value);
-  localStorage.setItem("childsQty", childsQty.value);
-}
-  if (!checkOutDate.value) {
     evt.preventDefault();
-    console.log('Укажите дату выезда!');
-  }
-  else {
+    console.log("Укажите дату заезда!");
+  } else {
     localStorage.setItem("adultsQty", adultsQty.value);
     localStorage.setItem("childsQty", childsQty.value);
   }
-  if (!adultsQty.value || adultsQty.value <=0) {
+  if (!checkOutDate.value) {
+    evt.preventDefault();
+    console.log("Укажите дату выезда!");
+  } else {
+    localStorage.setItem("adultsQty", adultsQty.value);
+    localStorage.setItem("childsQty", childsQty.value);
+  }
+  if (!adultsQty.value || adultsQty.value <= 0) {
     evt.preventDefault();
     console.log("Укажите количество взрослых гостей!");
-  }
-  else {
+  } else {
     localStorage.setItem("adultsQty", adultsQty.value);
     localStorage.setItem("childsQty", childsQty.value);
   }
   if (childsQty.value < 0) {
     evt.preventDefault();
     console.log("Укажите корректное значение количества детей!");
-
   }
-
-
 });
 
-
-
-
 function initMap() {
-  var myLatLng = {lat: 34.876142, lng: -111.756358};
+  var myLatLng = { lat: 34.876142, lng: -111.756358 };
 
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 9,
     center: myLatLng,
     disableDefaultUI: true
@@ -70,6 +61,6 @@ function initMap() {
   var marker = new google.maps.Marker({
     position: myLatLng,
     map: map,
-    title: 'Welcome to Sedona!'
+    title: "Welcome to Sedona!"
   });
 }
