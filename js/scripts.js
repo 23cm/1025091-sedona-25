f8522dd0e96eb913a03271b2925b1e124cae96f6
Max T <max@23cm.io>
2019-06-18 17:27:30 +0600

    анимация формы

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,11 @@ var hotelSearchButton = document.querySelector(".booking-promo-button");
 var searchPopup = document.querySelector(".booking-form");
 var form = searchPopup.querySelector("form");
 var checkInDate = form.querySelector("#checkInDate");
+var checkInDateError = form.querySelector("#checkIn");
 var checkOutDate = form.querySelector("#checkOutDate");
+var checkOutDateError = form.querySelector("#checkOut");
 var adultsQty = form.querySelector("#adultsQty");
+var adultsQtyError = form.querySelector("#adultsInfo");
 var childsQty = form.querySelector("#childsQty");
 
 var isStorageSupport = true;
@@ -24,21 +27,45 @@ hotelSearchButton.addEventListener("click", function(evt) {
 form.addEventListener("submit", function(evt) {
   if (!checkInDate.value) {
     evt.preventDefault();
+    searchPopup.classList.add("form-error-warning");
+    setTimeout(function() {
+      searchPopup.classList.remove("form-error-warning");
+    }, 800);
+    checkInDateError.classList.add("form-error");
     console.log("Укажите дату заезда!");
+    checkInDate.addEventListener("input", function() {
+      if (checkInDate.value) {
+        checkInDateError.classList.remove("form-error");
+      }
+    });
   } else {
     localStorage.setItem("adultsQty", adultsQty.value);
     localStorage.setItem("childsQty", childsQty.value);
   }
+
   if (!checkOutDate.value) {
     evt.preventDefault();
+    checkOutDateError.classList.add("form-error");
     console.log("Укажите дату выезда!");
+    checkOutDate.addEventListener("input", function() {
+      if (checkOutDate.value) {
+        checkOutDateError.classList.remove("form-error");
+      }
+    });
   } else {
     localStorage.setItem("adultsQty", adultsQty.value);
     localStorage.setItem("childsQty", childsQty.value);
   }
+
   if (!adultsQty.value || adultsQty.value <= 0) {
     evt.preventDefault();
+    adultsQtyError.classList.add("form-error");
     console.log("Укажите количество взрослых гостей!");
+    adultsQty.addEventListener("input", function() {
+      if (adultsQty.value) {
+        adultsQtyError.classList.remove("form-error");
+      }
+    });
   } else {
     localStorage.setItem("adultsQty", adultsQty.value);
     localStorage.setItem("childsQty", childsQty.value);
